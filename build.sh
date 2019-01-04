f7f3b2cd3e13ed3065ff1751bc9f9717e2845219
takenoko <takenoko.tech@gmail.com>
2019-01-04 22:37:06 +0900

    add unity

---
+++ build.sh
@@ -0,0 +1,6 @@
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+  -projectPath ./movie_flutter_unity \
+  -executeMethod BuildClass.AppBuild \
+  -batchmode \
+  -quit \
+  -logFile ./temp/log.txt
