d4fc943f113b4dd4c824b0e86a196642b7691a60
Takenoko <takenoko.tech@gmail.com>
2019-01-07 22:48:19 +0900

    fix

---
+++ movie_flutter_unity/Assets/Time.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Time : MonoBehaviour {
+
+	public Text ClockText;
+
+	// Use this for initialization
+	void Start () { }
+
+	// Update is called once per frame
+	void Update () {
+		ClockText.text = DateTime.Now.ToString ("HH:mm:ss");
+	}
+}
